43d556000711a63af0b1b7a26c6f753cd5bd1872
Hrampell <109776225+Hrampell@users.noreply.github.com>
2025-04-06 16:20:33 -0700

    Update secret.sh

--- secret.sh
+++ secret.sh
@@ -1,97 +1,65 @@
-#!/usr/bin/env python2
-import os
-import sys
-import time
-import subprocess
-import threading
-import urllib2
-from distutils.spawn import find_executable
+#!/usr/bin/env ruby
+require 'open-uri'
+require 'thread'
 
-# Define DEVNULL equivalent for Python 2.
-DEVNULL = open(os.devnull, 'wb')
+# Define the base URL and audio file location.
+rick = "https://keroserene.net/lol"
+audio_raw_url = "#{rick}/roll.s16"
+audio_file = "/tmp/roll.s16"
 
-def download_audio(audio_raw_url, audio_file):
-    if not os.path.exists(audio_file):
-        try:
-            print "Downloading audio from {} ...".format(audio_raw_url)
-            response = urllib2.urlopen(audio_raw_url)
-            data = response.read()
-            response.close()
-            with open(audio_file, 'wb') as f:
-                f.write(data)
-            print "Download complete."
-        except Exception as e:
-            print "Error downloading audio:", e
-            sys.exit(1)
+# Download the audio file if it does not exist.
+unless File.exist?(audio_file)
+  puts "Downloading audio file from #{audio_raw_url}..."
+  open(audio_raw_url) do |remote_file|
+    File.open(audio_file, "wb") do |local_file|
+      local_file.write(remote_file.read)
+    end
+  end
+  puts "Download complete."
+end
 
-def which(cmd):
-    return find_executable(cmd)
+# Method to set the system volume to maximum.
+def adjust_volume
+  if system("command -v osascript > /dev/null 2>&1")
+    system("osascript -e 'set volume output volume 100'")
+  elsif system("command -v amixer > /dev/null 2>&1")
+    system("amixer set Master 100%")
+  else
+    puts "No supported volume control found."
+  end
+end
 
-def adjust_volume():
-    # For macOS: set volume to maximum using osascript.
-    if which("osascript"):
-        subprocess.call(["osascript", "-e", "set volume output volume 100"],
-                        stdout=DEVNULL, stderr=DEVNULL)
-    # For Linux: set Master volume to 100% using amixer.
-    elif which("amixer"):
-        subprocess.call(["amixer", "set", "Master", "100%"],
-                        stdout=DEVNULL, stderr=DEVNULL)
-    else:
-        print "No supported volume control found."
+# Start a background thread to adjust volume every 3 seconds.
+volume_thread = Thread.new do
+  loop do
+    adjust_volume
+    sleep 3
+  end
+end
 
-def volume_loop(stop_event):
-    while not stop_event.is_set():
-        adjust_volume()
-        time.sleep(3)
+# Method to play one audio instance using an available player.
+def play_audio_instance(audio_file)
+  if system("command -v afplay > /dev/null 2>&1")
+    return Process.spawn("nohup afplay #{audio_file} >/dev/null 2>&1")
+  elsif system("command -v aplay > /dev/null 2>&1")
+    command = "cat #{audio_file} | aplay -Dplug:default -q -f S16_LE -r 8000"
+    return Process.spawn("nohup bash -c '#{command}' >/dev/null 2>&1")
+  elsif system("command -v play > /dev/null 2>&1")
+    return Process.spawn("nohup play -q #{audio_file} >/dev/null 2>&1")
+  else
+    puts "No supported audio player found."
+    exit 1
+  end
+end
 
-def play_audio_instance(audio_file):
-    # Try macOS afplay first.
-    if which("afplay"):
-        return subprocess.Popen(["nohup", "afplay", audio_file],
-                                stdout=DEVNULL, stderr=DEVNULL)
-    elif which("aplay"):
-        command = "cat {} | aplay -Dplug:default -q -f S16_LE -r 8000".format(audio_file)
-        return subprocess.Popen(["nohup", "bash", "-c", command],
-                                stdout=DEVNULL, stderr=DEVNULL)
-    elif which("play"):
-        return subprocess.Popen(["nohup", "play", "-q", audio_file],
-                                stdout=DEVNULL, stderr=DEVNULL)
-    else:
-        print "No supported audio player found."
-        sys.exit(1)
+# Spawn 5 overlapping audio instances with a 5-second delay between each.
+audio_pids = []
+5.times do |i|
+  puts "Starting audio instance #{i+1}..."
+  pid = play_audio_instance(audio_file)
+  audio_pids << pid
+  sleep 5
+end
 
-def main():
-    rick = "https://keroserene.net/lol"
-    audio_raw_url = "{}/roll.s16".format(rick)
-    audio_file = "/tmp/roll.s16"
-
-    # Download the audio file if it does not exist.
-    download_audio(audio_raw_url, audio_file)
-
-    # Start the volume-setting thread.
-    stop_event = threading.Event()
-    vol_thread = threading.Thread(target=volume_loop, args=(stop_event,))
-    vol_thread.setDaemon(True)
-    vol_thread.start()
-
-    # Launch 5 overlapping audio instances with a 5-second delay between each.
-    processes = []
-    for i in range(5):
-        print "Starting audio instance {}...".format(i + 1)
-        proc = play_audio_instance(audio_file)
-        processes.append(proc)
-        time.sleep(5)
-
-    # Keep the script running indefinitely.
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        print "Stopping audio processes and volume thread..."
-        stop_event.set()
-        for proc in processes:
-            proc.kill()
-        sys.exit(0)
-
-if __name__ == "__main__":
-    main()
+# Keep the script running indefinitely so the volume thread stays active.
+volume_thread.join
